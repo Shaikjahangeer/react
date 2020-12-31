f79d4d46d45a8f20f1141b392bf9f74822695acb
Shaikjahangeer <shaikjahangeer24@gmail.com>
2020-12-31 11:37:45 +0530

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,28 @@
+const Data = [
+    {
+        sname: "DARK",
+        imgsrc:"https://occ-0-4619-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABRSGrqLtrmo0Hn9wN2wqkfifrcQC-H9Zi_RpLjvSbRe5lY8_4id5kC9qWk24bs3fNzCTpRf4fo4jCKdleJPwZ1mYyw74ox2L5Czae7nr2YUyFaVpHj0DNXnWAvGD.jpg?r=393",
+        watchme :"https://www.netflix.com/in/title/80100172",
+    },
+    {
+        sname: "Money Heist",
+        imgsrc:"https://occ-0-4619-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABWN2gs7Ph0YCvz-pvnA2gio0pwYOM3T3jJ8xt3lPF7UsPpobqUexfddqZVp40Cu_Xy27q1liBPHrdIKGmy9p16Uf4C-n9aq5nnRU7licS5JjC2UXUCLG-RTezxqC.jpg?r=373",
+        watchme:"https://www.netflix.com/in/title/80192098",
+    },
+    {
+        sname: "Extra curricular",
+        imgsrc: "https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg",
+        watchme :"https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname: "Vampire Dairies",
+        imgsrc:"https://occ-0-4619-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABfXO86BDwXmv7PEXypu50vvdU1FqmBIEQ6OrxQlNvApT6uP8hPdNRJQnhAXjuXFmmDnRpSdvdNRZXCvxYjNMbCQSV40.jpg?r=262",    
+        watchme:"https://www.netflix.com/in/title/70143860",   
+     },
+     {
+        sname: "Stranger Things",
+        imgsrc:"https://occ-0-4619-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXMHsJCyHKyf3X3oEmYczFvXsgIiNdSK2bMf3pXy17SGgjGp_Utp8F4BRP-ui192RsgKQxZCzvkGVuI2N7Z0Rgh5a48_HMeLuEAc1TDK-8PjJBLwLlDAmAEwMlYX.jpg?r=da1",    
+        watchme:"https://www.netflix.com/in/title/80057281",   
+     }
+]
+export default Data
